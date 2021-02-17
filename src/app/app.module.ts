cca388cf9c38ad1be325cc39d554350d5dec55e8
nourhan <nourhan.mohammed227@gmail.com>
2021-02-17 22:52:30 +0200

    3amlt components bs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddRestaurantComponent } from './components/add-restaurant/add-restaurant.component';
+import { UpdateRestaurantComponent } from './components/update-restaurant/update-restaurant.component';
+import { RestaurantsListComponent } from './components/restaurants-list/restaurants-list.component';
+import { AddHotelComponent } from './components/add-hotel/add-hotel.component';
+import { HotelComponent } from './components/update/hotel/hotel.component';
+import { HotelsListComponent } from './components/hotels-list/hotels-list.component';
+import { UpdateHotelComponent } from './components/update-hotel/update-hotel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRestaurantComponent,
+    UpdateRestaurantComponent,
+    RestaurantsListComponent,
+    AddHotelComponent,
+    HotelComponent,
+    HotelsListComponent,
+    UpdateHotelComponent
   ],
   imports: [
     BrowserModule,
