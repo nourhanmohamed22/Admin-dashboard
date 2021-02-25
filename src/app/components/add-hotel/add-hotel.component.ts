7a18d981fe773062f604ae88f7547482de192cee
nourhan <nourhan.mohammed227@gmail.com>
2021-02-25 17:04:54 +0200

    style checkbox

--- src/app/components/add-hotel/add-hotel.component.ts
+++ src/app/components/add-hotel/add-hotel.component.ts
@@ -1,4 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { Api2Service } from './../../shared/api2.service';
+import { Api3Service } from './../../shared/api3.service';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
+export interface Distance {
+  mainStreet: number,
+  beach: number,
+  park: number,
+  cityCenter : number
+}
+export interface PriceDeals {
+  name: string,
+    link: string,
+    pricePerNight : number
+}
+export interface Map {
+  latitude: number;
+  longitude: number;
+}
 
 @Component({
   selector: 'app-add-hotel',
@@ -6,10 +29,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-hotel.component.css']
 })
 export class AddHotelComponent implements OnInit {
-
-  constructor() { }
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  @ViewChild('chipListHotel') chipList;
+  @ViewChild('resetHotelForm') myNgForm;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  hotelForm: FormGroup;
+images:string[];
+deals:string[];
+amenities:string[];
+style:string[];
+map:Map;
+rooms:number;
+distance:Distance;
+Pricedeals:PriceDeals[]=[];
+langaugeSpoken:string[];
+selected:string;
+  constructor(public fb: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+    private hotelApi: Api2Service) { }
 
   ngOnInit(): void {
+    this.HotelFormData();
+  }
+
+  HotelFormData() {
+    this.hotelForm = this.fb.group({
+      deals: [this.deals],
+      amenities: [this.amenities],
+      style: [this.style],
+      name: ['', [Validators.required]],
+      map: [this.map],
+      rooms:[, [Validators.required]],
+      distance:[this.distance],
+      Pricedeals:[this.Pricedeals],
+      langaugeSpoken:[this.langaugeSpoken]
+    })
+  }
+
+    /* Add dynamic styles */
+    // addStyle(event: MatChipInputEvent): void {
+    //   const input = event.input;
+    //   const value = event.value;
+    //   // Add style
+    //   if ((value || '').trim() && this.style.length < 5) {
+    //     this.style.push(value.trim())
+    //   }
+    //   // Reset the input value
+    //   if (input) {
+    //     input.value = '';
+    //   }
+    // }
+
+      /* Remove style */
+  // removeStyle(index:number): void {
+  //   const index = this.style.indexOf();
+  //   if (index >= 0) {
+  //     this.style.splice(index, 1);
+  //   }
+  // } 
+
+  
+  /* Get errors */
+  public handleError = (controlName: string, errorName: string) => {
+    return this.hotelForm.controls[controlName].hasError(errorName);
+  }  
+
+  /* Submit book */
+  submitHotelForm() {
+    if (this.hotelForm) {
+      this.hotelApi.AddHotel(this.hotelForm.value).subscribe(res => {
+        this.ngZone.run(() => this.router.navigateByUrl('/hotel-list'))
+      });
+    }
   }
 
 }
