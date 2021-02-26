7fd61ba0246a9020d8fa3822da1d61bc429ab773
Lama Mohamed <74375170+LamaMohamed@users.noreply.github.com>
2021-02-26 15:57:28 +0200

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,41 +35,5 @@ logout() {
 }
  
   ngOnInit() {
- /*    console.log(window.innerWidth)
-    if (window.innerWidth < 768) {
-      this.sidenav.fixedTopGap = 55;
-      this.opened = false;
-    } else {
-      this.sidenav.fixedTopGap = 55;
-      this.opened = true;
-    }
-    this.loading = true;
-    this.adminService.getAll().pipe(first()).subscribe(users => {
-        this.loading = false;
-        this.users = users;
-    }); */
-    
   }
-
- /*  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    if (event.target.innerWidth < 768) {
-      this.sidenav.fixedTopGap = 55;
-      this.opened = false;
-    } else {
-      this.sidenav.fixedTopGap = 55
-      this.opened = true;
-    }
-  }
-
-  isBiggerScreen() {
-    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if (width < 768) {
-      return true;
-    } else {
-      return false;
-    }
-  } */
-
- 
 }
